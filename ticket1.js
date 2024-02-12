0909a877a220d0c611b4d0af42706ef389f6c717
m.gajia1998@gmail.com <Gaajia.m.gajia1998@gmail.com>
2024-02-12 14:06:01 +0000

    conversionrate

---
+++ ticket1.js
@@ -0,0 +1,14 @@
+function convertGBPtoEUR(currency,conversionRate){  
+
+    currency * conversionRate 
+    
+    return
+
+
+
+};
+
+let gbp = 500;
+let eurorate = 1.17;
+
+console.log(convertGBPtoEUR(gbp, eurorate));
